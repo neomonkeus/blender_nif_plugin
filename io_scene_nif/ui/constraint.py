a73974fca3f4fa3aa9db1ea568bc04c17a82e526
neomonkeus <neomonkeus@gmail.com>
2020-05-07 19:36:13 +0100

    Create a new Constraint UI, shows only when there is a rigid_body_constraint.

---
+++ io_scene_nif/ui/constraint.py
@@ -0,0 +1,64 @@
+"""Nif User Interface, connect custom constraint properties from properties.py into Blenders UI"""
+
+# ***** BEGIN LICENSE BLOCK *****
+# 
+# Copyright © 2020, NIF File Format Library and Tools contributors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+# 
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+# 
+#    * Neither the name of the NIF File Format Library and Tools
+#      project nor the names of its contributors may be used to endorse
+#      or promote products derived from this software without specific
+#      prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ***** END LICENSE BLOCK *****
+
+from bpy.types import Panel
+
+
+class ConstraintPanel(Panel):
+    bl_idname = "NIFTOOLS_PT_ConstraintPanel"
+    bl_label = "Niftools Constraint Settings"
+
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "physics"
+
+    def draw_header(self, context):
+        if context.active_object.rigid_body_constraint:
+            return True
+        return False
+
+    def draw(self, context):
+        layout = self.layout
+
+        con_setting = context.active_object.niftools_constraint
+
+        box = layout.box()
+        box.prop(con_setting, "LHMaxFriction", text='LHMaxFriction')
+        box.prop(con_setting, "tau", text='tau')
+        box.prop(con_setting, "damping", text='Damping')
